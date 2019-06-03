fd7ebaf5a4d9600ced21c2fc2245b14e59f4bf7a
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-06-03 00:06:30 -0300

    chore: criação do projeto

---
+++ Jurify.Advogados.Api/Startup.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Jurify.Advogados.Api
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHealthChecks();
+            services.AddSwaggerDocument();
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHealthChecks("/health");
+            app.UseSwagger();
+            app.UseSwaggerUi3();
+            app.UseMvc();
+        }
+    }
+}
