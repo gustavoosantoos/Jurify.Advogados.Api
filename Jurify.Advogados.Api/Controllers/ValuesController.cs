30f86250378a3f624660e060c4a5906d5e249c31
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-06-04 23:38:30 -0300

    feat(authentication): adição do provedor de autenticação

--- Jurify.Advogados.Api/Controllers/ValuesController.cs
+++ Jurify.Advogados.Api/Controllers/ValuesController.cs
@@ -1,8 +1,10 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using System.Collections.Generic;
 
 namespace Jurify.Advogados.Api.Controllers
 {
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
--- Jurify.Advogados.Api/Startup.cs
+++ Jurify.Advogados.Api/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using static IdentityModel.OidcConstants;
 
 namespace Jurify.Advogados.Api
 {
@@ -17,6 +18,15 @@ namespace Jurify.Advogados.Api
 
         public void ConfigureServices(IServiceCollection services)
         {
+            services
+                .AddAuthentication(AuthenticationSchemes.AuthorizationHeaderBearer)
+                .AddIdentityServerAuthentication(options =>
+                {
+                    options.Authority = Configuration["Authentication:Authority"];
+                    options.RequireHttpsMetadata = false;
+                    options.ApiName = Configuration["Authentication:ResourceName"];
+                });
+
             services.AddHealthChecks();
             services.AddSwaggerDocument();
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
