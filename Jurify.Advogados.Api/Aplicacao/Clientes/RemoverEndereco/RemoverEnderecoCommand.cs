87f40539a10ec96a906e6b3bd1c7cbba7eb46615
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-07-21 22:39:56 -0300

    feat(clientes): inclusão do caso de uso para remover endereço

--- Jurify.Advogados.Api/Aplicacao/Clientes/RemoverEndereco/RemoverEnderecoCommand.cs
+++ Jurify.Advogados.Api/Aplicacao/Clientes/RemoverEndereco/RemoverEnderecoCommand.cs
@@ -1,6 +1,20 @@
-﻿namespace Jurify.Advogados.Api.Aplicacao.Clientes.RemoverEndereco
+﻿using Jurify.Advogados.Api.Infraestrutura.CasosDeUso.Comum;
+using MediatR;
+using System;
+
+namespace Jurify.Advogados.Api.Aplicacao.Clientes.RemoverEndereco
 {
-    public class RemoverEnderecoCommand
+    public class RemoverEnderecoCommand : IRequest<RespostaCasoDeUso>
     {
+        public RemoverEnderecoCommand()
+        {
+        }
+
+        public RemoverEnderecoCommand(Guid codigo)
+        {
+            Codigo = codigo;
+        }
+
+        public Guid Codigo { get; set; }
     }
 }
--- Jurify.Advogados.Api/Aplicacao/Clientes/RemoverEndereco/RemoverEnderecoCommandHandler.cs
+++ Jurify.Advogados.Api/Aplicacao/Clientes/RemoverEndereco/RemoverEnderecoCommandHandler.cs
@@ -1,6 +1,35 @@
-﻿namespace Jurify.Advogados.Api.Aplicacao.Clientes.RemoverEndereco
+﻿using Jurify.Advogados.Api.Infraestrutura.Autenticacao;
+using Jurify.Advogados.Api.Infraestrutura.CasosDeUso.Comum;
+using Jurify.Advogados.Api.Infraestrutura.Persistencia;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Jurify.Advogados.Api.Aplicacao.Clientes.RemoverEndereco
 {
-    public class RemoverEnderecoCommandHandler
+    public class RemoverEnderecoCommandHandler : BaseHandler, IRequestHandler<RemoverEnderecoCommand, RespostaCasoDeUso>
     {
+        public RemoverEnderecoCommandHandler(JurifyContext context, ProvedorUsuarioAtual provedor) : base(context, provedor)
+        {
+        }
+
+        public async Task<RespostaCasoDeUso> Handle(RemoverEnderecoCommand request, CancellationToken cancellationToken)
+        {
+            var endereco = await Context.Enderecos
+                .FirstOrDefaultAsync(e => e.Codigo == request.Codigo &&
+                                          e.CodigoEscritorio == Provedor.Escritorio.Codigo &&
+                                          !e.Apagado);
+
+            if (endereco == null)
+                return RespostaCasoDeUso.ComStatusCode(HttpStatusCode.NotFound);
+
+            Context.Enderecos.Remove(endereco);
+            await Context.SaveChangesAsync();
+
+            return RespostaCasoDeUso.ComSucesso();
+        }
     }
 }
