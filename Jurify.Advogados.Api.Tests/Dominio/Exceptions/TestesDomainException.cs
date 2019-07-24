41ba40e6bd416ae031d077d0be405c5b7309ef83
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-07-23 23:58:23 -0300

    tests(domain-exception): adiciona testes quando entidade ou objetos de valor forem nulos

--- Jurify.Advogados.Api.Tests/Dominio/Exceptions/TestesDomainException.cs
+++ Jurify.Advogados.Api.Tests/Dominio/Exceptions/TestesDomainException.cs
@@ -1,6 +1,9 @@
-﻿using Jurify.Advogados.Api.Dominio.Entidades;
+﻿using Jurify.Advogados.Api.Dominio.Base;
+using Jurify.Advogados.Api.Dominio.Entidades;
 using Jurify.Advogados.Api.Dominio.Enums;
 using Jurify.Advogados.Api.Dominio.Exceptions;
+using Jurify.Advogados.Api.Dominio.ObjetosDeValor;
+using System;
 using Xunit;
 
 namespace Jurify.Advogados.Api.Tests.Dominio.Exceptions
@@ -14,6 +17,29 @@ namespace Jurify.Advogados.Api.Tests.Dominio.Exceptions
             var exception = new DomainException(entidade);
 
             Assert.Equal(entidade.Notifications.Count, exception.Erros.Length);
+        }
+
+        [Fact]
+        public void ExcecaoDeveLancarArgumentNullExceptionQuandoEntidadeForNula()
+        {
+            Entidade entidade = null;
+            Assert.Throws<ArgumentNullException>(() => new DomainException(entidade));
+        }
+
+        [Fact]
+        public void ExcecaoDeveConterNotificacoesDoObjetoDeValor()
+        {
+            var objeto = new Nome(null, null);
+            var exception = new DomainException(objeto);
+
+            Assert.Equal(objeto.Notifications.Count, exception.Erros.Length);
+        }
+
+        [Fact]
+        public void ExcecaoDeveLancarArgumentNullExceptionQuandoObjetoDeValorForNulo()
+        {
+            ObjetoDeValor objeto = null;
+            Assert.Throws<ArgumentNullException>(() => new DomainException(objeto));
         }
     }
 }
--- Jurify.Advogados.Api/Dominio/Exceptions/DomainException.cs
+++ Jurify.Advogados.Api/Dominio/Exceptions/DomainException.cs
@@ -8,13 +8,19 @@ namespace Jurify.Advogados.Api.Dominio.Exceptions
     {
         public string[] Erros { get; private set; }
 
-        public DomainException(Entidade entidade) : base($"O objeto do tipo {entidade.GetType()} é inválido")
+        public DomainException(Entidade entidade) : base($"O objeto do tipo {entidade?.GetType()} é inválido")
         {
+            if (entidade == null)
+                throw new ArgumentNullException(nameof(entidade), "A entidade não deve ser nula");
+
             Erros = entidade.Notifications.Select(n => n.Message).ToArray();
         }
 
-        public DomainException(ObjetoDeValor objeto) : base($"O objeto do tipo {objeto.GetType()} é inválido")
+        public DomainException(ObjetoDeValor objeto) : base($"O objeto do tipo {objeto?.GetType()} é inválido")
         {
+            if (objeto == null)
+                throw new ArgumentNullException(nameof(objeto), "O objeto de valor não deve ser nulo");
+
             Erros = objeto.Notifications.Select(n => n.Message).ToArray();
         }
     }
