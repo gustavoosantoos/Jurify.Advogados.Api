fede2973cfc9c0c1d8261fe88e5042d4ab52c8a2
Gustavo Santos <gustavo.dev@outlook.com.br>
2019-07-23 23:47:39 -0300

    tests(domain-exception): adiciona testes para exceção de domínio

---
+++ Jurify.Advogados.Api.Tests/Dominio/Exceptions/TestesDomainException.cs
@@ -0,0 +1,19 @@
+﻿using Jurify.Advogados.Api.Dominio.Entidades;
+using Jurify.Advogados.Api.Dominio.Enums;
+using Jurify.Advogados.Api.Dominio.Exceptions;
+using Xunit;
+
+namespace Jurify.Advogados.Api.Tests.Dominio.Exceptions
+{
+    public class TestesDomainException
+    {
+        [Fact]
+        public void ExcecaoDeveConterNotificacoesDaEntidade()
+        {
+            var entidade = new Endereco(null, null, null, null, null, null, null, null, ETipoEndereco.Residencial);
+            var exception = new DomainException(entidade);
+
+            Assert.Equal(entidade.Notifications.Count, exception.Erros.Length);
+        }
+    }
+}
